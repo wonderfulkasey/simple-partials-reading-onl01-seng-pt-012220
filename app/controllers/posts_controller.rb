b83611d7abc698270fdd84c1e595fcc00c7b0740
Kasey Arnold <kaseyarnold15@gmail.com>
2020-06-23 17:09:53 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,7 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
-    @author = @post.author 
+    @author = @post.author
   end
 
   def new
@@ -31,5 +31,5 @@ class PostsController < ApplicationController
     @post.update(title: params[:title], description: params[:description])
     redirect_to post_path(@post)
   end
-  
+
 end
